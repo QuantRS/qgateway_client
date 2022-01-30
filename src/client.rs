929e40fd873342f900f59e1db0712b76455b0120
JinVan <admin@660.moe>
2022-01-30 18:43:01 +0800

    协议更新

--- src/client.rs
+++ src/client.rs
@@ -2,16 +2,17 @@
 use std::{collections::HashMap, sync::{Arc, Mutex}};
 
 use futures::{StreamExt, TryStreamExt, channel::mpsc::{self, UnboundedReceiver, UnboundedSender}, future, pin_mut};
-use serde::{Deserialize, Serialize};
-use serde_json::{Map, Value};
-use tokio_tungstenite::{connect_async, tungstenite::protocol::Message};
+use protobuf::Message;
+use tokio_tungstenite::{connect_async, tungstenite::protocol::Message as WSMessage};
 use once_cell::sync::OnceCell;
+
+use crate::protocol;
 
 pub struct Client {
     url: url::Url,
     auth_token: String,
-    queues: Arc<Mutex<HashMap<String, UnboundedSender<String>>>>,
-    send_channel: OnceCell<UnboundedSender<Message>>
+    queues: Arc<Mutex<HashMap<String, UnboundedSender<Vec<u8>>>>>,
+    send_channel: OnceCell<UnboundedSender<WSMessage>>
 }
 
 impl Client {
@@ -25,7 +26,7 @@ impl Client {
     }
 
     pub async fn close(&mut self) {
-        self.send_channel.get().unwrap().unbounded_send(Message::Close(None)).unwrap();
+        self.send_channel.get().unwrap().unbounded_send(WSMessage::Close(None)).unwrap();
     }
 
     pub async fn connection(&mut self) -> bool {
@@ -35,49 +36,63 @@ impl Client {
         let (tx, rx) = mpsc::unbounded();
         self.send_channel.set(tx.clone()).unwrap();
 
-        //login
-        tx.unbounded_send(Message::Text(serde_json::to_string(&Request{
-            cmd_id: 0,
-            args: Value::Array(vec![Value::String(self.auth_token.clone())])
-        }).unwrap())).unwrap();
+        // login
+        let mut req = protocol::Request::new();
+        req.set_command(protocol::Commands::LOGIN);
+
+        let mut login_req = protocol::LoginRequest::new();
+        login_req.set_token(self.auth_token.clone());
+
+        req.set_data(login_req.write_to_bytes().unwrap());
+
+        tx.unbounded_send(WSMessage::Binary(req.write_to_bytes().unwrap())).unwrap();
 
         let (wait_login_tx, mut wait_login_rx) = mpsc::unbounded();
 
         let queues_clone = self.queues.clone();
         tokio::spawn(async move {
             let broadcast_incoming = incoming.try_for_each(|msg| {
-                if msg.is_text() {
-                    let json = serde_json::from_str::<Response>(&msg.to_string());
-                    if !json.is_ok() {
-                        println!("{:?}", json.err().unwrap());
-    
-                        tx.unbounded_send(Message::Close(None)).unwrap();
-                        return future::ok(());
-                    }
+                match msg {
+                    WSMessage::Binary(msg) => {
+                        let res = protocol::Response::parse_from_bytes(&msg);
+                        if res.is_err() {
+                            println!("{:?}", res.err().unwrap());
 
-                    let res = json.unwrap();
-                    match res.cmd_id {
-                        0 => {
-                            if res.data.is_boolean() && res.data.as_bool().unwrap() {
-                                wait_login_tx.unbounded_send(true).unwrap();
-                                return future::ok(());
-                            }
-                            wait_login_tx.unbounded_send(false).unwrap();
-                        },
-                        2 => {
-                            if res.data.is_object() {
-                                let data = res.data.as_object().unwrap();
-                                if data.contains_key("token") && data.contains_key("value") {
-                                    for (token, queue) in queues_clone.lock().unwrap().iter() {
-                                        if data.get("token").unwrap().as_str().unwrap().eq(token) {
-                                            queue.unbounded_send(data.get("value").unwrap().as_str().unwrap().to_string()).unwrap();
-                                        }
+                            tx.unbounded_send(WSMessage::Close(None)).unwrap();
+                            return future::ok(());
+                        }
+
+                        let res = res.unwrap();
+                        match res.get_command() {
+                            protocol::Commands::HEARTBEAT => {
+                                // 心跳包
+                            },
+                            protocol::Commands::LOGIN => {
+                                let login_res = protocol::LoginResponse::parse_from_bytes(res.get_data()).unwrap();
+
+                                if login_res.get_status() {
+                                    wait_login_tx.unbounded_send(true).unwrap();
+                                    return future::ok(());
+                                }
+                                wait_login_tx.unbounded_send(false).unwrap();
+                            },
+                            protocol::Commands::SUBSCRIBE => {
+                                let subscribe_res = protocol::SubscribeResponse::parse_from_bytes(res.get_data()).unwrap();
+                                println!("{:?}", subscribe_res);
+                            },
+                            protocol::Commands::SUBSCRIBE_CALLBACK => {
+                                let callback = protocol::SubscribeCallback::parse_from_bytes(res.get_data()).unwrap();
+
+                                for (token, queue) in queues_clone.lock().unwrap().iter() {
+                                    if callback.get_token().eq(token) {
+                                        queue.unbounded_send(callback.get_data().to_vec()).unwrap();
                                     }
                                 }
-                            }
-                        },
-                        _ => {}
+                            },
+                            _ => {}
+                        }
                     }
+                    _ => {},
                 }
                 future::ok(())
             });
@@ -92,56 +107,35 @@ impl Client {
         result
     }
 
-    pub async fn subscribe(&mut self, token: String, keys: Vec<String>) -> UnboundedReceiver<String> {
+    pub async fn subscribe(&mut self, token: String, keys: Vec<String>) -> UnboundedReceiver<Vec<u8>> {
         let (tx, rx) = mpsc::unbounded();
         self.queues.clone().lock().unwrap().insert(token.clone(), tx);
 
-        let mut args = Map::new();
-        args.insert("token".to_string(), Value::String(token));
-        args.insert("keys".to_string(), Value::Array(keys.iter().map(|x| Value::String(x.to_string())).collect()));
+        let mut req = protocol::Request::new();
+        req.set_command(protocol::Commands::SUBSCRIBE);
 
-        let req = Request{
-            cmd_id: 2,
-            args: Value::Object(args)
-        };
+        let mut sub_req = protocol::SubscribeRequest::new();
+        sub_req.set_token(token);
+        sub_req.mut_keys().extend(keys);
 
-        self.send_channel.get().unwrap().unbounded_send(Message::Text(serde_json::to_string(&req).unwrap())).unwrap();
+        req.set_data(sub_req.write_to_bytes().unwrap());
+
+        self.send_channel.get().unwrap().unbounded_send(WSMessage::Binary(req.write_to_bytes().unwrap())).unwrap();
 
         return rx;
     }
 
-    pub fn send(&mut self, token: String, key: String, data: String) {
-        let mut args = Map::new();
-        args.insert("token".to_string(), Value::String(token));
-        args.insert("key".to_string(), Value::String(key));
-        args.insert("value".to_string(), Value::String(data));
-        let req = Request{
-            cmd_id: 1,
-            args: Value::Object(args)
-        };
+    pub fn send(&mut self, token: String, key: String, data: Vec<u8>) {
+        let mut req = protocol::Request::new();
+        req.set_command(protocol::Commands::SEND_MESSAGE);
 
-        self.send_channel.get().unwrap().unbounded_send(Message::Text(serde_json::to_string(&req).unwrap())).unwrap();
+        let mut send_req = protocol::SendRequest::new();
+        send_req.set_token(token);
+        send_req.set_key(key);
+        send_req.set_data(data);
+
+        req.set_data(send_req.write_to_bytes().unwrap());
+
+        self.send_channel.get().unwrap().unbounded_send(WSMessage::Binary(req.write_to_bytes().unwrap())).unwrap();
     }
-}
-
-#[derive(Debug, Clone, Serialize)]
-struct Request {
-    #[serde(rename = "cmdId")]
-    cmd_id: i32,
-    args: Value
-}
-
-
-#[derive(Debug, Clone, Deserialize)]
-struct Response {
-    #[serde(rename = "cmdId")]
-    cmd_id: i32,
-    #[serde(default = "fnull")]
-    data: Value,
-    #[serde(default = "fnull")]
-    message: Value
-}
-
-fn fnull() -> Value {
-    return Value::Null;
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,2 @@
+mod protocol;
 pub mod client;
